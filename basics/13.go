2c0c4c820c6b77355ef290dbd323145358473378
Rostislav Pylypiv <rostislav.pylypiv@gmail.com>
2022-08-29 12:27:16 +0300

    basics 13 added

---
+++ basics/13.go
@@ -0,0 +1,32 @@
+/*
+Перетворення типів
+
+Вираз T(v) перетворює значення v на тип T.
+
+Деякі числові перетворення:
+
+	var i int = 42
+	var f float64 = float64(i)
+	var u uint = uint(f)
+Або ще простіше:
+
+	i := 42
+	f := float64(i)
+	u := uint(f)
+На відміну від C, у Go призначення між елементами різного типу вимагає явного перетворення.
+Спробуйте видалити перетворення float64 або uint у прикладі та подивіться, що станеться.
+*/
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
+}
