47c7fd7801e6f117130396bea0d9adf341bc1bf6
Rostislav Pylypiv <rostislav.pylypiv@gmail.com>
2022-08-30 22:12:39 +0300

    flowcontrol 2 added

---
+++ flowcontrol/2.go
@@ -0,0 +1,17 @@
+/*
+Цикл For. Продовження
+
+Інструкції init і post необов'язкові.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+	}
+	fmt.Println(sum)
+}
